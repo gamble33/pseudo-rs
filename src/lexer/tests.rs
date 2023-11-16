c2e90a591290ef6b7c06b82fdbd8c53ae80a20d8
slopey <giafilmsyt@gmail.com>
2023-11-16 11:22:57 +0000

    change lexer tests to be more readable

--- src/lexer/tests.rs
+++ src/lexer/tests.rs
@@ -1,4 +1,5 @@
 use crate::lexer::{KeywordKind, Lexer, TokenKind, TokenLiteralKind};
+use TokenKind::*;
 
 fn check_lexing(src: &str, expect: Vec<TokenKind>) {
     let actual: Vec<TokenKind> = Lexer::new(src).map(|t| t.kind).collect();
@@ -7,18 +8,19 @@ fn check_lexing(src: &str, expect: Vec<TokenKind>) {
 
 #[test]
 fn smoke_test() {
+    use KeywordKind::*;
     check_lexing(
         "PROCEDURE Main
             OUTPUT \"Hello, World!\"
          ENDPROCEDURE",
         vec![
-            TokenKind::Keyword(KeywordKind::Procedure),
-            TokenKind::Identifier(String::from("Main")),
-            TokenKind::NewLine,
-            TokenKind::Keyword(KeywordKind::Output),
-            TokenKind::Literal(TokenLiteralKind::Str(String::from("Hello, World!"))),
-            TokenKind::NewLine,
-            TokenKind::Keyword(KeywordKind::EndProcedure),
+            Keyword(Procedure),
+            Identifier(String::from("Main")),
+            NewLine,
+            Keyword(Output),
+            Literal(TokenLiteralKind::Str(String::from("Hello, World!"))),
+            NewLine,
+            Keyword(EndProcedure),
         ],
     );
 }
@@ -28,9 +30,9 @@ fn characters() {
     check_lexing(
         "'a' ' ' '9'",
         vec![
-            TokenKind::Literal(TokenLiteralKind::Character('a')),
-            TokenKind::Literal(TokenLiteralKind::Character(' ')),
-            TokenKind::Literal(TokenLiteralKind::Character('9')),
+            Literal(TokenLiteralKind::Character('a')),
+            Literal(TokenLiteralKind::Character(' ')),
+            Literal(TokenLiteralKind::Character('9')),
         ],
     );
 }
@@ -49,8 +51,8 @@ fn comments() {
          PROCEDURE
          // Comment 3",
         vec![
-            TokenKind::Keyword(KeywordKind::Procedure),
-            TokenKind::NewLine,
+            Keyword(KeywordKind::Procedure),
+            NewLine,
         ],
     );
 }
@@ -60,13 +62,13 @@ fn new_lines() {
     check_lexing(
         "line1\r\nline2\nline3\rline4",
         vec![
-            TokenKind::Identifier(String::from("line1")),
-            TokenKind::NewLine,
-            TokenKind::Identifier(String::from("line2")),
-            TokenKind::NewLine,
-            TokenKind::Identifier(String::from("line3")),
-            TokenKind::NewLine,
-            TokenKind::Identifier(String::from("line4")),
+            Identifier(String::from("line1")),
+            NewLine,
+            Identifier(String::from("line2")),
+            NewLine,
+            Identifier(String::from("line3")),
+            NewLine,
+            Identifier(String::from("line4")),
         ],
     );
 }
@@ -76,92 +78,93 @@ fn multiple_new_lines() {
     check_lexing(
         "line1\r\n\r\n\r\nline2",
         vec![
-            TokenKind::Identifier(String::from("line1")),
-            TokenKind::NewLine,
-            TokenKind::Identifier(String::from("line2")),
+            Identifier(String::from("line1")),
+            NewLine,
+            Identifier(String::from("line2")),
         ],
     );
     check_lexing(
         "line1\r\r\r\r\r\r\n\r\n\n\n\r\n\n\n\n\r\n\n\r\rline2",
         vec![
-            TokenKind::Identifier(String::from("line1")),
-            TokenKind::NewLine,
-            TokenKind::Identifier(String::from("line2")),
+            Identifier(String::from("line1")),
+            NewLine,
+            Identifier(String::from("line2")),
         ],
     );
 }
 
 #[test]
 fn keywords() {
-    check_lexing("AND", vec![TokenKind::Keyword(KeywordKind::And)]);
-    check_lexing("OR", vec![TokenKind::Keyword(KeywordKind::Or)]);
-    check_lexing("NOT", vec![TokenKind::Keyword(KeywordKind::Not)]);
-    check_lexing("DIV", vec![TokenKind::Keyword(KeywordKind::Div)]);
-    check_lexing("MOD", vec![TokenKind::Keyword(KeywordKind::Mod)]);
-    check_lexing("DECLARE", vec![TokenKind::Keyword(KeywordKind::Declare)]);
-    check_lexing("CONSTANT", vec![TokenKind::Keyword(KeywordKind::Constant)]);
-    check_lexing("TYPE", vec![TokenKind::Keyword(KeywordKind::Type)]);
-    check_lexing("ENDTYPE", vec![TokenKind::Keyword(KeywordKind::EndType)]);
-    check_lexing("INTEGER", vec![TokenKind::Keyword(KeywordKind::Integer)]);
-    check_lexing("REAL", vec![TokenKind::Keyword(KeywordKind::Real)]);
-    check_lexing("CHAR", vec![TokenKind::Keyword(KeywordKind::Char)]);
-    check_lexing("STRING", vec![TokenKind::Keyword(KeywordKind::Str)]);
-    check_lexing("BOOLEAN", vec![TokenKind::Keyword(KeywordKind::Boolean)]);
-    check_lexing("DATE", vec![TokenKind::Keyword(KeywordKind::Date)]);
-    check_lexing("ARRAY", vec![TokenKind::Keyword(KeywordKind::Array)]);
-    check_lexing("OF", vec![TokenKind::Keyword(KeywordKind::Of)]);
-    check_lexing("BYREF", vec![TokenKind::Keyword(KeywordKind::ByRef)]);
-    check_lexing("BYVAL", vec![TokenKind::Keyword(KeywordKind::ByVal)]);
-    check_lexing("FUNCTION", vec![TokenKind::Keyword(KeywordKind::Function)]);
+    use KeywordKind::*;
+    check_lexing("AND", vec![Keyword(And)]);
+    check_lexing("OR", vec![Keyword(Or)]);
+    check_lexing("NOT", vec![Keyword(Not)]);
+    check_lexing("DIV", vec![Keyword(Div)]);
+    check_lexing("MOD", vec![Keyword(Mod)]);
+    check_lexing("DECLARE", vec![Keyword(Declare)]);
+    check_lexing("CONSTANT", vec![Keyword(Constant)]);
+    check_lexing("TYPE", vec![Keyword(Type)]);
+    check_lexing("ENDTYPE", vec![Keyword(EndType)]);
+    check_lexing("INTEGER", vec![Keyword(Integer)]);
+    check_lexing("REAL", vec![Keyword(Real)]);
+    check_lexing("CHAR", vec![Keyword(Char)]);
+    check_lexing("STRING", vec![Keyword(Str)]);
+    check_lexing("BOOLEAN", vec![Keyword(Boolean)]);
+    check_lexing("DATE", vec![Keyword(Date)]);
+    check_lexing("ARRAY", vec![Keyword(Array)]);
+    check_lexing("OF", vec![Keyword(Of)]);
+    check_lexing("BYREF", vec![Keyword(ByRef)]);
+    check_lexing("BYVAL", vec![Keyword(ByVal)]);
+    check_lexing("FUNCTION", vec![Keyword(Function)]);
     check_lexing(
         "ENDFUNCTION",
-        vec![TokenKind::Keyword(KeywordKind::EndFunction)],
+        vec![Keyword(EndFunction)],
     );
-    check_lexing("RETURN", vec![TokenKind::Keyword(KeywordKind::Return)]);
-    check_lexing("RETURNS", vec![TokenKind::Keyword(KeywordKind::Returns)]);
-    check_lexing("CALL", vec![TokenKind::Keyword(KeywordKind::Call)]);
+    check_lexing("RETURN", vec![Keyword(Return)]);
+    check_lexing("RETURNS", vec![Keyword(Returns)]);
+    check_lexing("CALL", vec![Keyword(Call)]);
     check_lexing(
         "PROCEDURE",
-        vec![TokenKind::Keyword(KeywordKind::Procedure)],
+        vec![Keyword(Procedure)],
     );
     check_lexing(
         "ENDPROCEDURE",
-        vec![TokenKind::Keyword(KeywordKind::EndProcedure)],
+        vec![Keyword(EndProcedure)],
     );
-    check_lexing("FOR", vec![TokenKind::Keyword(KeywordKind::For)]);
-    check_lexing("TO", vec![TokenKind::Keyword(KeywordKind::To)]);
-    check_lexing("STEP", vec![TokenKind::Keyword(KeywordKind::Step)]);
-    check_lexing("NEXT", vec![TokenKind::Keyword(KeywordKind::Next)]);
-    check_lexing("WHILE", vec![TokenKind::Keyword(KeywordKind::While)]);
-    check_lexing("ENDWHILE", vec![TokenKind::Keyword(KeywordKind::EndWhile)]);
-    check_lexing("REPEAT", vec![TokenKind::Keyword(KeywordKind::Repeat)]);
-    check_lexing("UNTIL", vec![TokenKind::Keyword(KeywordKind::Until)]);
-    check_lexing("IF", vec![TokenKind::Keyword(KeywordKind::If)]);
-    check_lexing("THEN", vec![TokenKind::Keyword(KeywordKind::Then)]);
-    check_lexing("ELSE", vec![TokenKind::Keyword(KeywordKind::Else)]);
-    check_lexing("ENDIF", vec![TokenKind::Keyword(KeywordKind::EndIf)]);
-    check_lexing("TRUE", vec![TokenKind::Keyword(KeywordKind::True)]);
-    check_lexing("FALSE", vec![TokenKind::Keyword(KeywordKind::False)]);
-    check_lexing("CASE", vec![TokenKind::Keyword(KeywordKind::Case)]);
+    check_lexing("FOR", vec![Keyword(For)]);
+    check_lexing("TO", vec![Keyword(To)]);
+    check_lexing("STEP", vec![Keyword(Step)]);
+    check_lexing("NEXT", vec![Keyword(Next)]);
+    check_lexing("WHILE", vec![Keyword(While)]);
+    check_lexing("ENDWHILE", vec![Keyword(EndWhile)]);
+    check_lexing("REPEAT", vec![Keyword(Repeat)]);
+    check_lexing("UNTIL", vec![Keyword(Until)]);
+    check_lexing("IF", vec![Keyword(If)]);
+    check_lexing("THEN", vec![Keyword(Then)]);
+    check_lexing("ELSE", vec![Keyword(Else)]);
+    check_lexing("ENDIF", vec![Keyword(EndIf)]);
+    check_lexing("TRUE", vec![Keyword(True)]);
+    check_lexing("FALSE", vec![Keyword(False)]);
+    check_lexing("CASE", vec![Keyword(Case)]);
     check_lexing(
         "OTHERWISE",
-        vec![TokenKind::Keyword(KeywordKind::Otherwise)],
+        vec![Keyword(Otherwise)],
     );
-    check_lexing("ENDCASE", vec![TokenKind::Keyword(KeywordKind::EndCase)]);
-    check_lexing("INPUT", vec![TokenKind::Keyword(KeywordKind::Input)]);
-    check_lexing("OUTPUT", vec![TokenKind::Keyword(KeywordKind::Output)]);
-    check_lexing("OPENFILE", vec![TokenKind::Keyword(KeywordKind::OpenFile)]);
-    check_lexing("READFILE", vec![TokenKind::Keyword(KeywordKind::ReadFile)]);
+    check_lexing("ENDCASE", vec![Keyword(EndCase)]);
+    check_lexing("INPUT", vec![Keyword(Input)]);
+    check_lexing("OUTPUT", vec![Keyword(Output)]);
+    check_lexing("OPENFILE", vec![Keyword(OpenFile)]);
+    check_lexing("READFILE", vec![Keyword(ReadFile)]);
     check_lexing(
         "WRITEFILE",
-        vec![TokenKind::Keyword(KeywordKind::WriteFile)],
+        vec![Keyword(WriteFile)],
     );
     check_lexing(
         "CLOSEFILE",
-        vec![TokenKind::Keyword(KeywordKind::CloseFile)],
+        vec![Keyword(CloseFile)],
     );
-    check_lexing("READ", vec![TokenKind::Keyword(KeywordKind::Read)]);
-    check_lexing("WRITE", vec![TokenKind::Keyword(KeywordKind::Write)]);
-    check_lexing("APPEND", vec![TokenKind::Keyword(KeywordKind::Append)]);
-    check_lexing("RANDOM", vec![TokenKind::Keyword(KeywordKind::Random)]);
+    check_lexing("READ", vec![Keyword(Read)]);
+    check_lexing("WRITE", vec![Keyword(Write)]);
+    check_lexing("APPEND", vec![Keyword(Append)]);
+    check_lexing("RANDOM", vec![Keyword(Random)]);
 }
